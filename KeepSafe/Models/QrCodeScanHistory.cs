e1023d46bc25626f80bc0353189c81678a08a17a
Lynuel Jan Arcenas <lynuel.arcenas@havaapp.ph>
2020-07-10 01:00:01 +0800

    Fixed response data parsing

---
+++ KeepSafe/Models/QrCodeScanHistory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace KeepSafe.Models
+{
+    public class QrCodeScanHistory : BaseNotify
+    {
+        string _Image;
+        public string Image
+        {
+            get { return _Image; }
+            set { _Image = value; OnPropertyChanged(); }
+        }
+
+        string _FirstName;
+        public string FirstName
+        {
+            get { return _FirstName; }
+            set { _FirstName = value; OnPropertyChanged(); }
+        }
+
+        string _LastName;
+        public string LastName
+        {
+            get { return _LastName; }
+            set { _LastName = value; OnPropertyChanged(); }
+        }
+
+        string _ScanDate;
+        public string ScanDate
+        {
+            get { return _ScanDate; }
+            set { _ScanDate = value; OnPropertyChanged(); }
+        }
+    }
+}
